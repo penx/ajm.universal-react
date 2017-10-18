64941e64397e32928c13975df83cf2421e45be95
Alasdair McLeay <alasdair.mcleay@me.com>
2017-10-18 18:10:56 +0100

    Example component prop from container

--- src/shared/components/example/example.js
+++ src/shared/components/example/example.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-export default () => (<div>
+const Example = ({ example }) => (<div>
   <h2>Example Component</h2>
+  <p>Prop: {example}</p>
   <Link to="/">Home</Link>
 </div>)
+
+Example.propTypes = {
+  example: PropTypes.string
+}
+
+Example.defaultProps = {
+  example: null
+}
+
+export default Example
